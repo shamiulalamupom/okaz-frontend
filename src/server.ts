0667a7369343e9907aa5104b65e56b61a83e866e
shamiulalamupom <1hourprograms@gmail.com>
2026-04-01 13:50:34 +0200

    refactor: starting frontend from port 5000.

--- src/server.ts
+++ src/server.ts
@@ -50,7 +50,7 @@ app.use((req, res, next) => {
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
 if (isMainModule(import.meta.url) || process.env['pm_id']) {
-  const port = process.env['PORT'] || 4000;
+  const port = process.env['PORT'] || 5000;
   app.listen(port, (error) => {
     if (error) {
       throw error;
@@ -64,4 +64,3 @@ if (isMainModule(import.meta.url) || process.env['pm_id']) {
  * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
  */
 export const reqHandler = createNodeRequestHandler(app);
-
